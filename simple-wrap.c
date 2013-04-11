53caba5fa2465f2807d80710bd4039589e5ef80b
Marek Kubica <marek@xivilization.net>
2013-04-11 16:32:50 +0200

    Added a lot of information about GC

--- simple-wrap.c
+++ simple-wrap.c
@@ -1,5 +1,8 @@
 CAMLprim value ost_version_number(value unit)
 {
+    CAMLparam1(unit);
+    CAMLlocal1(r);
     int version_number = archive_version_number();
-    return Val_int(version_number);
+    r = Val_int(version_number);
+    CAMLreturn(r);
 }
