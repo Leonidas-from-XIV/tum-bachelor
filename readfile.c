796ad0a12f5550178d49c0fbd575bcc7a9ea80b1
Marek Kubica <marek@xivilization.net>
2013-06-08 22:18:41 +0200

    Switched code example to one from K&R

--- readfile.c
+++ readfile.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+int fgetline(char* line, int max, FILE* f) {
+  if (fgets(line, max, f) == NULL)
+    return 0;
+  else
+    return strlen(line);
+}
 int main(int argc, char** argv) {
   FILE* f;
-  char* line;
-  size_t len;
+  char line[256];
 
   f = fopen("inputfile.txt", "r");
   if (!f) {
     fprintf(stderr, "Error opening file for reading.\n");
     return 1;
   }
-  while (line = fgetln(f, &len)) {
+  while (fgetline(line, 256, f)) {
     fputs("LINE: ", stdout);
-    fwrite(line, len, 1, stdout);
+    fwrite(line, strlen(line), 1, stdout);
   }
   fclose(f);
   return 0;
